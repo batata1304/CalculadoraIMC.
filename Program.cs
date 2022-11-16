e80f2abba6e88852656171f236bc31f6e39546d7
batata1304 <matheustenorio1304@gmail.com>
2022-11-16 17:14:59 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CalculadoraIMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Individuo individuo1 = new Individuo (66, 1.78);  
+            Console.WriteLine(individuo1.Classificar()); 
+            Console.WriteLine(individuo1.ReajusteDePeso());
+
+        }
+    }
+}
